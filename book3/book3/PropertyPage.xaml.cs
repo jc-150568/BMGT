1eeba3ea84da6c027d775abce740277acfc8b12b
裕登 鎌田 <150265@jc-21.jp>
2018-01-12 16:57:25 +0900

    本棚追加

---
+++ book3/book3/PropertyPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace book3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PropertyPage : ContentPage
+    {
+        public PropertyPage()
+        {
+            InitializeComponent();
+        }
+        
+        private void Common(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CommonPage());
+        }
+        private void Backup(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new BackupPage());
+        }
+        private void Help(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new HelpPage());
+        }
+        
+    }
+}
