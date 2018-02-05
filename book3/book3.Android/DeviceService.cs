f916eb8d648d785d7422b67688f4d931b3bfc2da
裕登 鎌田 <150265@jc-21.jp>
2018-02-06 02:27:00 +0900

    fix

---
+++ book3/book3.Android/DeviceService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using book3.Droid;
+
+[assembly: Dependency(typeof(DeviceService))]
+namespace book3.Droid
+{
+    public class DeviceService : IDeviceService
+    {
+        public void PlayVibrate()
+        {
+            var obj = Forms.Context.GetSystemService(Context.VibratorService);
+            Vibrator vibrator = (Vibrator)obj;
+            vibrator.Vibrate(500);
+        }
+    }
+}
---
+++ book3/book3.iOS/DeviceService.cs
@@ -0,0 +1,14 @@
+﻿using AudioToolbox;
+using Xamarin.Forms;
+using book3.iOS.Services;
+[assembly: Dependency(typeof(DeviceService))]
+namespace book3.iOS.Services
+{
+    public class DeviceService : IDeviceService
+    {
+        public void PlayVibrate()
+        {
+            SystemSound.Vibrate.PlayAlertSound();
+        }
+    }
+}
