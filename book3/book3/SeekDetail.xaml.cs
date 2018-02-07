9a391e4d959ad7d3b2911ce6b3ce2c5038552864
遼太郎 金 <150568@jc-21.jp>
2018-02-07 17:25:29 +0900

    dsf

---
+++ book3/book3/SeekDetail.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace book3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SeekDetail : ContentPage
+    {
+        public SeekDetail()
+        {
+            InitializeComponent();
+        }
+    }
+}
